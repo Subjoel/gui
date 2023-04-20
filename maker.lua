7bd0d52fc5669547bfd90fff183732ac8d173e79
sub joel <89309881+Subjoel@users.noreply.github.com>
2023-04-20 17:29:20 +0200

    Create maker.lua

---
+++ maker.lua
@@ -0,0 +1,7 @@
+function ButtonClick()
+    task.wait(1)
+    game.CoreGui.notify:Destroy()
+    --place your function for what needs to happen when they click the button below this
+    
+end
+_G.Text = '' --your notification text inside the ''
